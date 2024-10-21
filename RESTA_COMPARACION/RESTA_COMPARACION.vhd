a78a4f2f9ee42e491c6aaed938e539cc8039f890
yaelrdf <mc.minion2005@hotmail.com>
2024-10-21 02:09:43 -0600

    Coment and pinout fixes

--- RESTA_COMPARACION/RESTA_COMPARACION.vhd
+++ RESTA_COMPARACION/RESTA_COMPARACION.vhd
@@ -4,39 +4,44 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity RESTA_COMPARACION is
+            --Entradas como vector de bits
     Port ( A : in  STD_LOGIC_VECTOR (3 downto 0);
            B : in  STD_LOGIC_VECTOR (3 downto 0);
+           ---Salidas
            PS : out STD_LOGIC;
            D : out STD_LOGIC_VECTOR (3 downto 0);
-           C1 : out STD_LOGIC;  -- A > B
-           C2 : out STD_LOGIC;  -- A < B
-           C3 : out STD_LOGIC); -- A = B
+           --Salidas comparador
+           C1, C2, C3 : out STD_LOGIC);
 end RESTA_COMPARACION;
 
 architecture Behavioral of RESTA_COMPARACION is
+    --Diferencia como signal
     signal diff : STD_LOGIC_VECTOR(4 downto 0);
 begin
-    -- Subtraction process
+    --Proceso para la resta utilizando diff como ayuda
     process(A, B)
     begin
         diff <= ('0' & A) - ('0' & B);
     end process;
 
-    -- Assign output
-    PS <= diff(4);  -- Sign bit (1 if negative)
-    D <= diff(3 downto 0);  -- Difference
+    ---Salidas bit de prestamo y resultado
+    PS <= diff(4);  -- Bit de prestamo
+    D <= diff(3 downto 0);  -- Diferencia o substraccion
 
-    -- Comparison process
+    -- Proceso de comparacion
     process(A, B)
     begin
+        ---Mayor que
         if (A > B) then
             C1 <= '1';
             C2 <= '0';
             C3 <= '0';
+        --Menor que
         elsif (A < B) then
             C1 <= '0';
             C2 <= '1';
             C3 <= '0';
+        -- Igual
         else
             C1 <= '0';
             C2 <= '0';
