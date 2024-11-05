4cdb86cce99bbf5451fbfa59d335a19f52c64895
yaelrdf <mc.minion2005@hotmail.com>
2024-11-04 21:29:50 -0600

    Binary to hex fixed

--- HEXA_BIN_EQU/HEXA_BIN_EQU.vhd
+++ HEXA_BIN_EQU/HEXA_BIN_EQU.vhd
@@ -9,12 +9,12 @@ end HEXA_BIN_EQU;
 
 architecture behavioral of HEXA_BIN_EQU is
 begin
-    Ad <= (A and C and not(B and D)) or (A and B and D and (not C)) or (B and not(A and C and D)) or (D and not(A and B and C));
-    Bd <= (A and C and D) or (B and C and(not D)) or (A and B and (not D)) or (B and D and not(A and C));
-    Cd <= (A and B and C) or (A and B and (not D)) or (not(A and B and D) and C);
-    Dd <= (B and C and D) or (A and C and not(D and B)) or (not(A and B and C) and D) or (B and not(A and C and D));
-    Ed <= (D and not A) or (not(B and C) and D) or (not(A and C) and B);
-    Fd <= (C and D and not(A)) or (not(A and B) and D) or (not(A and B) and C) or (A and B and D and not(C));
-    Gd<= (not(A and B and C)) or (A and B and(not (C and D))) or (not(C and D and B) and A);
+    Ad <= (not(a) and b and not(C) and not(D)) or (A and B and not(C) and D) or (A and not(B) and C and D) or (not(A) and not(B) and not(c) and D);
+    Bd <= (A and C and D) or (B and C and not(D)) or (not(A) and B and not(C) and D) or (A and B and not(D));
+    Cd <= (A and B and C) or (not(A) and not(B) and C and not(D)) or (A and B and not(D));
+    Dd <= (C and D and B) or (A and not(B) and C and not(D)) or (not(A) and B and not(C) and not(D)) or (not(A) and not(B) and not(C) and D);
+    Ed <= (not(A) and D) or (not(A) and B and not(C) and not(D)) or (B and not(C) and D);
+    Fd <= (C and not(A) and not(B)) or (not(A) and C and D) or (not(A) and not(B) and D) or (A and B and not(C) and D);
+    Gd<= (not(A) and not(B) and not(C)) or (not(A) and B and C and D) or (A and B and not(C) and not(D));
 
 end behavioral;
